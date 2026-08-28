286a90414b7d70ccfa8b9e829a0ef98e5e3f55d6
bylampa <157304712+bylampa@users.noreply.github.com>
2026-08-28 22:59:47 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -22,11 +22,9 @@ function checkSyncConflicts() {
     } else {
         $('div[data-name="acc_sync"]').show();
         $('div[data-name="acc_sync_disabled"]').remove();
-		setTimeout(function() {
-		   var F = document.querySelector("#app > div.settings.animate > div.settings__content.layer--height > div.settings__body > div > div > div > div > div:nth-child(5)")
-           Lampa.Controller.focus(F)
-           Lampa.Controller.toggle('settings_component')
-		}, 50);
+		var F = document.querySelector("#app > div.settings.animate > div.settings__content.layer--height > div.settings__body > div > div > div > div > div:nth-child(5)")
+        Lampa.Controller.focus(F)
+        Lampa.Controller.toggle('settings_component')
     }
     
     // Если включена синхронизация BYLAMPA, скрываем оригинальный параметр CUB и показываем заглушку
