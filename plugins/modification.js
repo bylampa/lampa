e1fde6fb84f6438a513a21cab8dd302bce56c745
bylampa <157304712+bylampa@users.noreply.github.com>
2026-08-29 11:20:24 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -104,7 +104,7 @@ Lampa.Settings.listener.follow('open', function(e) {
 });*/
 
 localStorage.setItem('cub_mirrors', '["cubleave.store"]');
-localStorage.setItem('cub_domain', 'cubleave.store');
+// localStorage.setItem('cub_domain', 'cubleave.store');
 //localStorage.removeItem('cub_domain');
 
 Lampa.SettingsApi.addParam({
